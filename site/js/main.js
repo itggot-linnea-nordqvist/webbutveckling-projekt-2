1c813e6fb7c40a968197913b91aa3e3fb22a8474
itggot-linnea-nordqvist <linnea.nordqvist@elev.ga.ntig.se>
2019-01-10 15:44:49 +0100

    home page

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,11 @@
 function knapp(event){
-    let thing=document.querySelector(".wrapper")
-    thing.classList.toggle("activate")
     let menu=document.querySelector("nav")
     menu.classList.toggle("test")
+    
+}
+
+function lista(event){
+    let thing=document.querySelector("#hej")
+    thing.classList.toggle("activate")
     
 }
